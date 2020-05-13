75317b1d20123d147cf7c623ac4eb86b60e2f53e
rmnkweb <rkulkov.web@gmail.com>
2020-05-13 14:55:41 +0300

    Added UserController add(...) function.

--- www/app/controllers/UserController.php
+++ www/app/controllers/UserController.php
@@ -11,4 +11,27 @@ class UserController extends Controller {
     public function defaultAction() {
         $users = new User();
     }
+
+    public function add($username, $password, $group = 0) {
+        if ((!empty($username)) AND (!empty($password))) {
+            $values = [
+                "name" => $username,
+                "password" => $password,
+                "group" => $group
+            ];
+            $users = new User();
+            if ($users->checkConstraints($values)) {
+                try {
+                    $users->addUser($values);
+                    return true;
+                } catch (\Exception $exception) {
+                    echo $exception->getMessage();
+                    return false;
+                }
+            } else {
+                echo "Constraints failure." . PHP_EOL;
+                return false;
+            }
+        }
+    }
 }
