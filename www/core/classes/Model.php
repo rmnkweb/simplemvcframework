851b05bc8b65691f7069ef7b5b7c73ec64569b61
rmnkweb <rkulkov.web@gmail.com>
2020-05-13 14:54:26 +0300

    Model now can use Constraints class functionality.

--- www/core/classes/Model.php
+++ www/core/classes/Model.php
@@ -4,6 +4,7 @@ namespace Core;
 use PDO;
 use PDOException;
 use Exception;
+use Core\Constraints as Constraints;
 
 
 class Model {
@@ -233,6 +234,22 @@ class Model {
         } else {
             return false;
         }
+    }
+
+    public function checkConstraints($values) {
+        $constraints = new Constraints;
+        $noError = true;
+        foreach ($this->fields as $fieldName => $field) {
+            if ((array_key_exists($fieldName, $values)) AND ($noError)) {
+                if (array_key_exists("constraints", $field)) {
+                    if ($constraints->check($field["constraints"], $values[$fieldName]) === false) {
+                        $noError = false;
+                    }
+                }
+            }
+        }
+
+        return $noError;
     }
 
 }
